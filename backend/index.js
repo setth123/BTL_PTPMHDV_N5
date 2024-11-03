2a74206dd83228533373d38229e29b05dfaffbfa
tp4071 <phamtinh4071@gmail.com>
2024-11-03 21:48:40 +0700

    update server

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ import interestRateByIdRouter from "./route/interestRateByIdRouter.js";
 import carVerByCarIdRouter from "./route/carVerByCarIdRouter.js";
 import cors from 'cors'
 const app=express();
+app.use(cors());
 app.use('/backend',rateRouter,carRouter,carVerRouter,
                    carByIdRouter,carVerByIdRouter,interestRateByIdRouter,carVerByCarIdRouter);
-app.use(cors());
 mongoose.connect("mongodb+srv://thanhtkcb2004:ksiuOWOBVmMF6sP5@cluster0.uuuqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
     console.log('yes');
