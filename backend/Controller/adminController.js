97b2728bb8ce7450a1766a56a37f167ab11793dd
Edward <thanhtkcb2004@gmail.com>
2024-11-06 22:49:30 +0700

    Fix login

--- backend/Controller/adminController.js
+++ backend/Controller/adminController.js
@@ -5,10 +5,10 @@ import jwt from "jsonwebtoken";
 
 const JWT_Secret='Sep21th2thousand4';
 export const login=async(req,res)=>{
-    const {username,password}=req.body();
+    const {username,password}=req.body;
     try{
-        const admin=await Admin.findOne({username:username});
-        if(!user)return res.status(400).send("Invalid username or password");
+        const admin=await Admin.findOne({username});
+        if(!admin)return res.status(400).send("Admin not found");
         const isMatch=await bcrypt.compare(password,admin.password);
         if(!isMatch)return res.status(400).send("Invalid username or password");
 
--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ import verifyToken from "./middlewares/verify.js";
 const app=express();
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 //verifyToken to verify admin before doing any actions
 app.use('/rate',verifyToken,rateRouter);
 app.use('/car',verifyToken,carRouter);
