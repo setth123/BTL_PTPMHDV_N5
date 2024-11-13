9d8f5fd74a2dcdf976bdf556d8e2c0c5534b90ca
TrangDiep04 <diepkieutrang2004@gmail.com>
2024-11-13 13:29:46 +0700

    Update carRouters.js add to del

--- backend/route/carRouters.js
+++ backend/route/carRouters.js
@@ -2,6 +2,7 @@ import express from "express";
 import Car from "../Models/Car.js"
 import editRecordService from "../Services/dataService/editRecordService.js";
 import getDataService from "../Services/dataService/getDataService.js";
+import delRecordService from "../Services/dataService/delRecordService.js"
 const carRouter=express.Router();
 
 carRouter.get('/',async(req,res)=>{
@@ -32,4 +33,18 @@ carRouter.put('/editcar/:tableName/:id', async(req,res)=>{
         res.status(500).json({message: error.message});
     }
 })
+
+// xoa theo id 
+carRouter.delete('/:id', async (req, res) => {
+  const id = req.params.id; // Lấy ID từ params
+  try {
+      const deletedRecord = await delRecordService('Car', id); // Gọi hàm xóa
+      if (!deletedRecord) {
+          return res.status(404).json({ message: `Không tìm thấy xe với ID ${id}` });
+      }
+      return res.status(200).json({ message: "Xe đã được xóa thành công", deletedRecord });
+  } catch (error) {
+      return res.status(500).json({ message: error.message });
+  }
+});
 export default carRouter;
