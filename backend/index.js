836a3b5e27d5a87fdb71d2803a30e63f73b5b4b2
Edward Laurent <thanhtkcb2004@gmail.com>
2024-12-25 23:34:27 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,28 +23,8 @@ app.use('/carVer',verifyToken, carVerRouters);
 app.use('/admin',adminRouters);
 app.use('/custormerStatic',verifyToken,customerStaticRouter);
 
-async function setDefaultValues() {
-    try {
-      await Car.updateMany(
-        { viewed: { $exists: false } }, // Chỉ cập nhật những bản ghi chưa có field "status"
-        { $set: { viewed:0  } }
-      );
-      await InterestRate.updateMany(
-        {viewed:{$exists:false}},
-        {$set:{viewed:0}}
-      )
-      await carVersion.updateMany(
-        {viewed:{$exists:false}},
-        {$set:{viewed:0}}
-      )
-    } catch (error) {
-      console.error('Lỗi khi cập nhật giá trị mặc định:', error);
-    }
-  }
-
 mongoose.connect("mongodb+srv://thanhtkcb2004:ksiuOWOBVmMF6sP5@cluster0.uuuqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(async()=>{
-    setDefaultValues();
     console.log('yes');
     app.listen(3000,'0.0.0.0', ()=>{
         console.log("success");
