1fc8c5910a03de9a600df323f5a73ad6d69adccc
Edward Laurent <thanhtkcb2004@gmail.com>
2024-12-27 01:11:43 +0700

    Delete FE directory

--- FE/assets/js/templateLoader.js
+++
@@ -1,5 +0,0 @@
-function loadTemplate(templateId) {
-    const template = document.getElementById(templateId);
-    const content = document.getElementById('content');
-    content.innerHTML = template.innerHTML;
-}
